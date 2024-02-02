5fb7f63c220c539ac72faf6100ec9cb7b8e74139
Kamal Huseynaliyev <kamalrh@code.edu.az>
2024-02-02 17:47:10 +0400

    Add project files.

---
+++ 02-02-24/Exam.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_02_24
+{
+    internal class Exam
+    {
+        public string Subject;
+        public DateTime Date;
+        public Exam(string subject, DateTime date)
+        {
+            this.Subject = subject;
+            this.Date = date;
+        }
+
+        public static List<Exam> exams = new List<Exam>();
+        
+        public static void AddNewExam()
+        {
+            string subject;
+            do
+            {
+                Console.Write("Enter subject: ");
+                subject = Console.ReadLine();
+            } while (String.IsNullOrWhiteSpace(subject));
+
+            string dateStr;
+            DateTime date;
+            do
+            {
+                Console.Write("Enter exam date: ");
+                dateStr = Console.ReadLine();
+            } while (!DateTime.TryParse(dateStr, out date));
+
+            if (!exams.Exists(x=>x.Subject == subject))
+            {
+                exams.Add(new Exam(subject, date));
+                Console.WriteLine("Exam added successfully!");
+                
+            }
+            else
+            {
+                Console.WriteLine("Exam already exists!");
+            }
+            
+        }
+
+        public static void ShowExamsStartingWithA()
+        {
+            var filteredExams = FilterExams(exam => exam.Subject.StartsWith("A"));
+            DisplayExams(filteredExams);
+        }
+
+        public static void ShowPassedExams()
+        {
+            var filteredExams = FilterExams(exam => exam.Date < DateTime.Now);
+            DisplayExams(filteredExams);
+        }
+
+        public static void ShowExamsInLessThanOneMonth()
+        {
+            var filteredExams = FilterExams(exam => exam.Date.Month < DateTime.Now.Month + 1 && exam.Date > DateTime.Now);
+            DisplayExams(filteredExams);
+        }
+
+        public static void RemovePassedExams()
+        {
+            exams.RemoveAll(exam => exam.Date < DateTime.Now);
+            Console.WriteLine("Passed exams removed successfully!");
+        }
+
+        public static void CheckIfMathExamExists()
+        {
+            bool exists = exams.Any(exam => exam.Subject.Equals("Riyaziyyat", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine($"Math exam exists: {exists}");
+        }
+
+        public static void ShowMathExams()
+        {
+            var filteredExams = FilterExams(exam => exam.Subject.Equals("Riyaziyyat", StringComparison.OrdinalIgnoreCase));
+            DisplayExams(filteredExams);
+        }
+
+        private static List<Exam> FilterExams(Func<Exam,bool> abc)
+        {
+            return exams.Where(abc).ToList();
+        }
+        
+        private static void DisplayExams(List<Exam> exams)
+        {
+            foreach (var exam in exams)
+            {
+                Console.WriteLine($"Subject: {exam.Subject}, Date: {exam.Date.Date}");
+            }
+        }
+    }
+}
---
+++ 02-02-24/Program.cs
@@ -0,0 +1,58 @@
+﻿using _02_02_24;
+using System.Collections.Generic;
+
+
+
+string opt;
+do
+{
+    Console.WriteLine("1. Add new exam");
+    Console.WriteLine("2. Show exams starting with 'A'");
+    Console.WriteLine("3. Show passed exams");
+    Console.WriteLine("4. Show exams happening in less than 1 month");
+    Console.WriteLine("5. Show exams at 08:00");
+    Console.WriteLine("6. Remove passed exams");
+    Console.WriteLine("7. Check if there is a Math exam");
+    Console.WriteLine("8. Show Math exams");
+    Console.WriteLine("0. Exit");
+
+    Console.WriteLine("Enter the operation number:");
+    opt = Console.ReadLine();
+
+    switch (opt)
+    {
+        case "1":
+            Exam.AddNewExam();
+            break;
+        case "2":
+            Exam.ShowExamsStartingWithA();
+            break;
+        case "3":
+            Exam.ShowPassedExams();
+            break;
+        case "4":
+            Exam.ShowExamsInLessThanOneMonth();
+            break;
+        case "5":
+            
+            break;
+        case "6":
+            Exam.RemovePassedExams();
+            break;
+        case "7":
+            Exam.CheckIfMathExamExists();
+            break;
+        case "8":
+            Exam.ShowMathExams();
+            break;
+        case "0":
+            Console.WriteLine("Program Finished");
+            break;
+        default:
+            Console.WriteLine("Invalid option. Please try again.");
+            break;
+    }
+
+
+} while (opt != "0");
+
